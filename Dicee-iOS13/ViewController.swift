c9b118e45b89c2b66f1c0105c02d9bf15e888a6a
Ben Ringia <benringia@gmail.com>
2020-08-30 16:29:23 +0800

    +randomize , fixed launchScreen

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Allows me to reference UI Elements
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 1
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceNumbers = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+//        let randomize = Int.random(in: 0...5)
+        
+        diceImageView1.image =  diceNumbers.randomElement()
+        diceImageView2.image =  diceNumbers.randomElement()
+        
+    }
 }
 
